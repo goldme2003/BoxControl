f952deb92556d95db20784e22c6f3801f7effee1
zhenxin <goldme2003@sina.com>
2016-06-06 15:15:27 +0800

    Initial Commit

---
+++ app/src/main/java/com/nari/boxcontrol/MainActivity.java
@@ -0,0 +1,13 @@
+package com.nari.boxcontrol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
